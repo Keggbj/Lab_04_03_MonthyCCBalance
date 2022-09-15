527ac5ed68466e0923aa7c15814052d8f1b9d604
minik <keggbj@mail.uc.edu>
2022-09-15 17:09:57 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         double creditCard = 5_000;
-        int interestOneMonth = creditCard * 0.17;
-        int interestTwoMonth = interestOneMonth * 0.17;
+        double interestOneMonth = creditCard * 0.17;
+        double interestTwoMonth = (creditCard + interestOneMonth) * 0.17;
 
         System.out.println("The calculated interest after one month is: " + interestOneMonth);
 
-        System.out.println("The calculated interest after one month is: " + interestTwoMonth);
+        System.out.println("The calculated interest after two months is: " + interestTwoMonth);
     }
 }
